6a5ea9ab5182540289f9406134223905af7aa38b
Renn <906155099@qq.com>
2023-09-20 22:53:02 +0800

    chore: 修改unocss预设

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,16 @@
 // uno.config.ts
-import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import { defineConfig, presetAttributify, presetUno } from "unocss";
 
 export default defineConfig({
+  rules: [
+    // ...custom rules
+    ["flex-center", { display: "flex", "justify-content": "center", "align-items": "center" }],
+  ],
   presets: [
-    presetAttributify({ /* preset options */}),
+    presetAttributify({
+      /* preset options */
+    }),
     presetUno(),
     // ...custom presets
   ],
-})
+});
